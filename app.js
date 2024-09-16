c1777fce60f356b8c7cd6c51ac1cfa675bfd18bb
Gerald <gerald.carigand@gmail.com>
2024-09-16 16:21:47 +0200

    refactorisation du cade

--- app.js
+++ app.js
@@ -2,39 +2,21 @@ document.getElementById("myForm").addEventListener("submit", myFunction);
 
 function myFunction(e) {
     e.preventDefault();
-    console.log("Je recupere  l'events");
+    console.log("Je recupere l'events");
 
     //Je recupère la valeur du champ prénom du formulaire
-    let balisefirstName = document.getElementById("first-name")
-    let firstname = balisefirstName.value
+    let firstNamevalue = document.getElementById("first-name").value;
+    let lastNamevalue = document.getElementById("last-name").value;
+    let messageValue = document.getElementById("message").value;
+    
 
        //Je vérifie si le champ firstName est vide
-    if (firstname =="") {
-            console.log("peut pas etre vide")
-        }else{
-            console.log(firstname); // affiche ce qui est contenu dans la balise Firstname
-        }
-
-    //Je recupère la valeur du champ nom du formulaire
-    let baliselastName = document.getElementById("last-name")
-    let lastname = baliselastName.value
+         if (firstNamevalue =="" || lastNamevalue=="" || messageValue =="") {
+            console.log("Ne peut pas etre vide")
+            document.getElementById("error-message").style.display="block";
+         }
+         else{
+            console.log(firstNamevalue, lastNamevalue, messageValue); // affiche ce qui est contenu dans la balise Firstname
+         }
       
-    //Je vérifie si le champ lastName est vide
-    if (lastname =="") {
-        console.log("peut pas etre vide")
-    }else{
-        console.log(lastname); // affiche ce qui est contenu dans la balise LastName
-    }
-
-    //Je recupère la valeur du champ message du formulaire
-    let balisemessage = document.getElementById("message")
-    let message = balisemessage.value
-
-        //Je vérifie si le champ message est vide
-        if (message =="") {
-            console.log("peut pas etre vide")
-        }else{
-            console.log(message); // affiche ce qui est contenu dans la balise Message
-        }
-  
 }
